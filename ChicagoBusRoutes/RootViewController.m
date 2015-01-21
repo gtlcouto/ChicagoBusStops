de82ddaf73bfbbf3d1e259af4f84c5cd61a8c1fe
Gustavo Couto <gtlcouto@Gustavos-MacBook-Pro.local>
2015-01-21 13:45:38 -0800

    Included iCarousel

--- ChicagoBusRoutes/RootViewController.m
+++ ChicagoBusRoutes/RootViewController.m
@@ -12,12 +12,17 @@
 #import "Parser.h"
 #import "DetailViewController.h"
 #import "CustomAnnotation.h"
+#import "iCarousel.h"
 
-@interface RootViewController () <MKMapViewDelegate, UITableViewDataSource, UITableViewDelegate>
+@interface RootViewController () <MKMapViewDelegate, UITableViewDataSource, UITableViewDelegate, iCarouselDataSource, iCarouselDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property NSMutableArray * annotationArray;
 @property CustomAnnotation * currentAnnotation;
+@property iCarousel * annotationCarousel;
+@property (weak, nonatomic) IBOutlet UIView *carouselItemView;
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
 
 @end
 
@@ -30,13 +35,17 @@
     [self loadPinsFromArray:self.annotationArray];
     self.currentAnnotation = [CustomAnnotation new];
     self.mapView.delegate = self;
+    self.annotationCarousel.delegate = self;
+    self.annotationCarousel.type = iCarouselTypeCoverFlow2;
+    [self.annotationCarousel reloadData];
+    //self.annotationCarousel.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"busStop.png"]];
+
 }
 
 #pragma mark - MapView Delegate Methods
 
 -(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
 {
-
     MKPinAnnotationView * pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil];
     pin.canShowCallout = YES;
     pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
@@ -79,15 +88,56 @@ calloutAccessoryControlTapped:(UIControl *)control
     [self performSegueWithIdentifier:@"detailSegue" sender:tableView];
 }
 
+#pragma mark - Carousel Delegate
+
+- (NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel
+{
+    //return the total number of items in the carousel
+    return self.annotationArray.count;
+}
+
+- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view
+{
+   	UIButton *button = (UIButton *)view;
+    if (button == nil)
+    {
+        //no button available to recycle, so create new one
+        //UIImage *image = [UIImage imageNamed:@"busStop.png"];
+        button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.frame = CGRectMake(0.0f, 0.0f, 200, 200);
+        [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        //[button setBackgroundImage:image forState:UIControlStateNormal];
+        [button setBackgroundColor:[UIColor grayColor]];
+        [button addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
+        button.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        button.titleLabel.textAlignment = NSTextAlignmentCenter;
+    }
+
+    //set button label
+    CustomAnnotation * myAnnotation = [CustomAnnotation new];
+    myAnnotation = self.annotationArray[index];
+//    [button setTitle:[NSString stringWithFormat:@"%@/n%@",myAnnotation.stopName, myAnnotation.routes ]];
+    [button setTitle:[NSString stringWithFormat:@"%@\n%@",myAnnotation.stopName, myAnnotation.routes] forState:UIControlStateNormal];
+
+    return button;
+}
+
 #pragma mark - IBAction Methods
 - (IBAction)onSegmentedControlChanged:(UISegmentedControl *)sender {
     if (sender.selectedSegmentIndex == 0) {
         self.mapView.hidden = NO;
         self.tableView.hidden = YES;
+        self.annotationCarousel.hidden = YES;
     } else if (sender.selectedSegmentIndex == 1)
     {
         self.mapView.hidden = YES;
         self.tableView.hidden = NO;
+        self.annotationCarousel.hidden = YES;
+    } else if (sender.selectedSegmentIndex == 2)
+    {
+        self.mapView.hidden = YES;
+        self.tableView.hidden = YES;
+        self.annotationCarousel.hidden = NO;
     }
 }
 
@@ -112,6 +162,13 @@ calloutAccessoryControlTapped:(UIControl *)control
     [self.mapView showAnnotations:self.mapView.annotations animated:YES];
 }
 
+- (void)buttonTapped:(UIButton *)sender
+{
+
+    [self performSegueWithIdentifier:@"detailSegue" sender:sender];
+
+}
+
 #pragma mark - Segue Methods
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -125,9 +182,12 @@ calloutAccessoryControlTapped:(UIControl *)control
         NSIndexPath *myIndexPath = [self.tableView
                                     indexPathForSelectedRow];
         dvc.selectedAnnotation = [self.annotationArray objectAtIndex:myIndexPath.row];
+    } else if([sender isKindOfClass:[UIButton class]])
+    {
+        //get item index for button
+        NSInteger index = [self.annotationCarousel indexOfItemViewOrSubview:sender];
+        dvc.selectedAnnotation = self.annotationArray[index];
     }
-
-    
 
 }
 
